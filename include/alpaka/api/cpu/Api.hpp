4798727303d1e489c2c4f95e0b1172af0eab2407
René Widera <r.widera@hzdr.de>
2025-02-26 16:33:08 +0100

    fix CPU GetArchSimdWidth
    - fix division by zero
    - add definition for __riscv_vector

--- include/alpaka/api/cpu/Api.hpp
+++ include/alpaka/api/cpu/Api.hpp
@@ -54,35 +54,23 @@ namespace alpaka
         {
             constexpr uint32_t operator()(api::Cpu const) const
             {
-                constexpr std::size_t simdWidthInByte =
-#if defined(__AVX512BW__)
-                    // addition (AVX512BW): vpaddb / _mm512_mask_add_epi8
-                    // subtraction (AVX512BW): vpsubb / _mm512_sub_epi8
-                    // multiplication: -
+                constexpr uint32_t simdWidthInByte =
+#if defined(__AVX512BW__) || defined(__AVX512F__) || defined(__AVX512DQ__) || defined(__AVX512VL__)
                     64u;
+#elif defined(__riscv_vector)
+                    64u
 #elif defined(__AVX2__)
-                    // addition (AVX2): vpaddb / _mm256_add_epi8
-                    // subtraction (AVX2): vpsubb / _mm256_sub_epi8
-                    // multiplication: -
                     32u;
-#elif defined(__SSE2__)
-                    // addition (SSE2): paddb / _mm_add_epi8
-                    // subtraction (SSE2): psubb / _mm_sub_epi8
-                    // multiplication: -
+#elif defined(__SSE__) || defined(__SSE2__) || defined(__SSE4_1__) || defined(__SSE4_2__)
                     16u;
 #elif defined(__ARM_NEON__)
                     16u;
 #elif defined(__ALTIVEC__)
                     16u;
-#elif defined(__CUDA_ARCH__)
-                    // addition: __vadd4
-                    // subtraction: __vsub4
-                    // multiplication: -
-                    4u;
 #else
-                    1u;
+                    sizeof(T_Type);
 #endif
-                return simdWidthInByte / sizeof(T_Type);
+                return std::max(static_cast<uint32_t>(simdWidthInByte / sizeof(T_Type)), 1u);
             }
         };
 
