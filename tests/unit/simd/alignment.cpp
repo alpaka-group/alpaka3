69af0151471bcc11bf83f9ddb8ca6cefdfd3fbff
René Widera <r.widera@hzdr.de>
2025-07-02 09:48:00 +0200

    fix SYCL warning
    ```
    tests/unit/simd/alignment.cpp:50:23: warning: 'alignof' applied to an expression is a GNU extension [-Wgnu-alignof-expression]
       50 |         static_assert(alignof(s6) == 1u);
    ```
    
    `alignof()` requires a type and not a variable.

--- tests/unit/simd/alignment.cpp
+++ tests/unit/simd/alignment.cpp
@@ -23,21 +23,21 @@ struct SimdAlignment
         using namespace alpaka;
 
         constexpr auto s1 = Simd{3, 7, 4};
-        static_assert(alignof(s1) == 4u);
+        static_assert(alignof(ALPAKA_TYPEOF(s1)) == 4u);
 
         constexpr auto s2 = Simd<double, 3u>{3., 7., 4.};
-        static_assert(alignof(s2) == 8u);
+        static_assert(alignof(ALPAKA_TYPEOF(s2)) == 8u);
 
         constexpr auto v1 = Vec{1.f, 2.f, 3.f};
         constexpr auto s3 = Simd{v1, v1, v1, v1};
-        static_assert(alignof(s3) == 4u);
+        static_assert(alignof(ALPAKA_TYPEOF(s3)) == 4u);
 
         constexpr auto v2 = Vec{1.f, 2.f};
         constexpr auto s4 = Simd{v2, v2, v2};
-        static_assert(alignof(s4) == 4u);
+        static_assert(alignof(ALPAKA_TYPEOF(s4)) == 4u);
 
         constexpr auto s5 = Simd{v2, v2, v2, v2};
-        static_assert(alignof(s5) == 32u);
+        static_assert(alignof(ALPAKA_TYPEOF(s5)) == 32u);
 
         struct Foo
         {
@@ -47,7 +47,7 @@ struct SimdAlignment
         };
 
         constexpr auto s6 = Simd{Foo{'a', 'b', 'c'}, Foo{'d', 'e', 'f'}, Foo{'g', 'h', 'i'}};
-        static_assert(alignof(s6) == 1u);
+        static_assert(alignof(ALPAKA_TYPEOF(s6)) == 1u);
     }
 };
 
