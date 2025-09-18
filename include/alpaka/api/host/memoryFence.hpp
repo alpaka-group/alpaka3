d87261643dbe586900f60e23c86568e43995a0d6
René Widera <r.widera@hzdr.de>
2025-09-18 12:45:04 +0200

    fence test refactoring
    - use atomic to signal errors
    - use always volatile, also for host accelerated code
    - disable `tsan` for fence tests

--- include/alpaka/api/host/memoryFence.hpp
+++ include/alpaka/api/host/memoryFence.hpp
@@ -3,24 +3,72 @@
  */
 
 #pragma once
+
 #include "alpaka/api/host/Api.hpp"
 #include "alpaka/api/host/executor.hpp"
 #include "alpaka/api/host/tag.hpp"
 #include "alpaka/core/common.hpp"
 #include "alpaka/onAcc/Acc.hpp"
+#include "alpaka/onAcc/memoryScope.hpp"
+#include "alpaka/tag.hpp"
 
 #include <atomic>
 #include <type_traits>
 
 namespace alpaka::onAcc::internalCompute
 {
-    // Host API: all scopes map to the same host atomic fence
+    namespace detail
+    {
+        // suppress warning: `warning: 'atomic_thread_fence' is not supported with '-fsanitize=thread' [-Wtsan]`
+#if defined(__GNUC__) && !defined(__clang__)
+#    pragma GCC diagnostic push
+#    pragma GCC diagnostic ignored "-Wtsan"
+#endif
+
+        // Serial executor fence implementation
+        // Block scope: nothing to do for serial
+        inline void hostMemoryFenceImpl(exec::CpuSerial const&, memoryScope::Block const)
+        {
+            // Block scope: NO-OP since threads within a block
+        }
+
+        inline void hostMemoryFenceImpl(exec::CpuSerial const&, memoryScope::Device const)
+        {
+            std::atomic_thread_fence(std::memory_order_acq_rel);
+        }
+
+        inline void hostMemoryFenceImpl(exec::CpuSerial const&, memoryScope::System const)
+        {
+            std::atomic_thread_fence(std::memory_order_acq_rel);
+        }
+
+        inline void hostMemoryFenceImpl(exec::CpuOmpBlocks const&, memoryScope::Block const)
+        {
+            // Block scope: NO-OP for OMP since single-threaded within a block
+        }
+
+        inline void hostMemoryFenceImpl(exec::CpuOmpBlocks const&, memoryScope::Device const)
+        {
+            std::atomic_thread_fence(std::memory_order_acq_rel);
+        }
+
+        inline void hostMemoryFenceImpl(exec::CpuOmpBlocks const&, memoryScope::System const)
+        {
+            std::atomic_thread_fence(std::memory_order_acq_rel);
+        }
+
+#if defined(__GNUC__) && !defined(__clang__)
+#    pragma GCC diagnostic pop
+#endif
+    } // namespace detail
+
+    // Host API: dispatch to executor-specific implementation
     template<typename T_Scope>
     struct MemoryFence::Op<api::Host, T_Scope>
     {
-        constexpr void operator()(onAcc::concepts::Acc<api::Host> auto const&, T_Scope const) const
+        void operator()(onAcc::concepts::Acc<api::Host> auto const& acc, T_Scope const scope) const
         {
-            std::atomic_thread_fence(std::memory_order_acq_rel);
+            detail::hostMemoryFenceImpl(acc[object::exec], scope);
         }
     };
 } // namespace alpaka::onAcc::internalCompute
