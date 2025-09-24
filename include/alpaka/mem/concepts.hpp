1b63b604ce5fdb9cf6c27fbb178563f080da6a66
Simeon Ehrig <s.ehrig@hzdr.de>
2025-09-24 08:44:31 +0200

    documented IMdSpan, IView and IBuffer interface concepts

--- include/alpaka/mem/concepts.hpp
+++ include/alpaka/mem/concepts.hpp
@@ -35,6 +35,43 @@ namespace alpaka::concepts
 
     namespace impl
     {
+        /** Interface concept that describes how multidimensional memory can be accessed on the host and device side.
+         *
+         * @details
+         *
+         * An object of type `alpaka::mdspan` does not store any information about the storage location, e.g., whether
+         * the memory is located on a CPU or a GPU. The interface corresponds to that of a standard library container
+         * with continuous memory, but has some differences to support multidimensional memory. For example, instead of
+         * the member function `size()`, which returns the 1D size, `alpaka::mdspan` like objects provides the function
+         *`getExtents()`, which returns the size of each dimension.
+         *
+         * @param t Object of type `alpaka::mdspan`. May or may not have a const modifier.
+         * @param mut_t Mutable object of type `alpaka::mdspan`. Does not have a const modifier.
+         * @param const_t Constant object of type `alpaka::mdspan`. Does have a const modifier.
+         * @param vec Vector with the same number of elements as the dimension of the `alpaka::mdspan` like object.
+         * Used to call the access operator.
+         *
+         *  @section components Components
+         *
+         * An `alpaka::mdspan` like object contains 4 components:
+         * - A pointer to the actual memory.
+         * - An extents object that describes the number of dimensions and their respective sizes.
+         * - A pitch object that specifies how many bytes are required to jump to the next element in each dimension.
+         * - An alignment object that describes how the elements are aligned in memory, see:
+         * alpaka::concepts::Alignment
+         *
+         * @section membertypes Member types
+         * - <b>T::value_type</b>: The element type. Can be const or not.
+         * - <b>T::reference</b>: The element reference type is either const or non-const, depending on
+         *`T::value_type`.
+         * - <b>T::const_reference</b>: The constant reference type for an element. Always const.
+         * - <b>T::pointer</b>: The element pointer type is either const or non-const, depending on
+         *`T::value_type`.
+         * - <b>T::const_pointer</b>: The constant pointer type for an element. Always const.
+         * - <b>T::index_type</b>: The index type of the pitch.
+         *
+         * @note The access operator [] with an integral as an argument is only available if the dimension is one.
+         **/
         template<typename T, typename MutT, typename ConstT>
         concept IMdSpan = requires(T t, MutT mut_t, ConstT const_t, alpaka::Vec<uint32_t, T::dim()> vec) {
             typename T::value_type;
@@ -55,7 +92,7 @@ namespace alpaka::concepts
             { *const_t } -> std::same_as<typename T::const_reference>;
             { mut_t.data() } -> std::same_as<typename T::pointer>;
             { const_t.data() } -> std::same_as<typename T::const_pointer>;
-            // TODO(SimeonEhrig): check for a MDIterator concept
+            /// @todo check for a MDIterator concept
             t.begin();
             t.end();
             t.cbegin();
@@ -71,19 +108,48 @@ namespace alpaka::concepts
                 { const_t[0] } -> std::same_as<typename T::const_reference>;
             };
 
-            // TODO(SimeonEhrig): add getSlice, getConstSlice and getView, getConstView functions
+            /// @todo add getSlice, getConstSlice and getView, getConstView functions
 
             { t.getAlignment() } -> alpaka::concepts::Alignment;
             t.getExtents();
             t.getPitches();
         };
 
+        /** Interface concept that describes how multidimensional memory can be accessed on the host and device side.
+         * An `alpaka::view` like object contains information about the device(s) to which it is connected. The
+         * `alpaka::view` like object has no memory ownership, and therefore it does not manage the memory lifetime.
+         *
+         * The concept fulfills all requirements of the alpaka::concepts::impl::IMdSpan concept and therefore offers
+         * the same interface.
+         *
+         **/
         template<typename T, typename MutT, typename ConstT>
         concept IView = requires(T t) {
             requires IMdSpan<T, MutT, ConstT>;
+            // TODO(SimeonEhrig): { t.getApi() } -> alpaka::concepts::Api
+            // cannot find concept
+            // looks like I have care about the correct ordering of concept includes
             t.getApi();
         };
 
+        /** Interface concept that describes how multidimensional memory can be accessed on the host and device side.
+         * An `alpaka::buffer` like object contains information about the device(s) to which it is connected. The
+         * `alpaka::buffer` like object has memory ownership and therefore manages memory lifetime according to the
+         * RAII principle.
+         *
+         * The concept fulfills all requirements of the alpaka::concepts::impl::IView concept and therefore offers
+         * the same interface.
+         *
+         * @section memberfunction member functions
+         *
+         * - <b>t.addDestructorAction</b>: Adds a destructor action to the shared buffer.
+         * @code{.unparsed}
+         *    The action will be executed when the buffer is destroyed.
+         *    This can be used to add additional cleanup actions e.g. waiting on a specific queue.
+         *    Actions are executed in FIFO order.
+         * @endcode
+         * - <b>t.destructorWaitFor</b>: Add an action to be executed when the shared_ptr is destroyed.
+         **/
         template<typename T, typename MutT, typename ConstT>
         concept IBuffer = requires(T t) {
             requires IView<T, MutT, ConstT>;
@@ -92,6 +158,14 @@ namespace alpaka::concepts
         };
     } // namespace impl
 
+    /** Interface concept that describes how multidimensional memory can be accessed on the host and device side.
+     * An object of type `alpaka::mdspan` does not store any information about the storage location, e.g., whether
+     * the memory is located on a CPU or a GPU.
+     *
+     * \attention Use `alpaka::IMdSpan` to restrict types in your code. The actual interface is described in
+     * alpaka::concepts::impl::IMdSpan.
+     *
+     **/
     template<typename T, typename T_ValueType = alpaka::NotRequired>
     concept IMdSpan = requires {
         requires impl::IMdSpan<
@@ -102,6 +176,14 @@ namespace alpaka::concepts
     };
 
 
+    /** Interface concept that describes how multidimensional memory can be accessed on the host and device side.
+     * An `alpaka::view` like object contains information about the device(s) to which it is connected. The
+     * `alpaka::view` like object has no memory ownership, and therefore it does not manage the memory lifetime.
+     *
+     * \attention Use `alpaka::IView` to restrict types in your code. The actual interface is described in
+     * alpaka::concepts::impl::IView.
+     *
+     **/
     template<typename T, typename T_ValueType = alpaka::NotRequired>
     concept IView = requires(T t) {
         requires impl::IView<
@@ -111,6 +193,15 @@ namespace alpaka::concepts
         requires ExpectedValueType<trait::GetValueType_t<std::decay_t<T>>, T_ValueType>;
     };
 
+    /** Interface concept that describes how multidimensional memory can be accessed on the host and device side.
+     * An `alpaka::buffer` like object contains information about the device(s) to which it is connected. The
+     * `alpaka::buffer` like object has memory ownership and therefore manages memory lifetime according to the RAII
+     * principle.
+     *
+     * \attention Use `alpaka::IBuffer` to restrict types in your code. The actual interface is described in
+     * alpaka::concepts::impl::IBuffer.
+     *
+     **/
     template<typename T, typename T_ValueType = alpaka::NotRequired>
     concept IBuffer = requires(T t) {
         requires impl::IBuffer<
