197504ef310003af4d420b33b7e1363a02ea02c0
Simeon Ehrig <s.ehrig@hzdr.de>
2025-09-29 11:38:16 +0200

    implement copy and move constructor for alpaka::mdspan which respects the const correctness of the data type
    - We allow to copy/move data sources with the following inner data types to other objects with the same type with the corresponding (different) inner data type
      - mutable -> mutable
      - const -> const
      - mutable -> const
    
    - Only alpaka::mdspan is implemented at the moment. alpaka::sharedBuffer and alpaka::View is missing.
    - Only compile tests yet. Didn't check if the `alpaka::mdspan` is moved instead copied.

--- include/alpaka/mem/trait.hpp
+++ include/alpaka/mem/trait.hpp
@@ -11,30 +11,61 @@
 
 #include <cstdint>
 
-namespace alpaka::onAcc::internal
+namespace alpaka
 {
-    namespace trait
+    namespace onAcc::internal
     {
-        struct AutoIndexMapping
+        namespace trait
         {
-            template<typename T_Acc, typename T_Api, alpaka::deviceKind::concepts::DeviceKind T_DeviceKind>
-            struct Op
+            struct AutoIndexMapping
             {
-                constexpr auto operator()(T_Acc const&, T_Api, T_DeviceKind) const
+                template<typename T_Acc, typename T_Api, alpaka::deviceKind::concepts::DeviceKind T_DeviceKind>
+                struct Op
                 {
-                    return layout::Strided{};
-                }
+                    constexpr auto operator()(T_Acc const&, T_Api, T_DeviceKind) const
+                    {
+                        return layout::Strided{};
+                    }
+                };
             };
-        };
-    } // namespace trait
+        } // namespace trait
 
-    constexpr auto adjustMapping(auto const& acc)
+        constexpr auto adjustMapping(auto const& acc)
+        {
+            return trait::AutoIndexMapping::
+                Op<ALPAKA_TYPEOF(acc), ALPAKA_TYPEOF(acc.getApi()), ALPAKA_TYPEOF(acc.getDeviceKind())>{}(
+                    acc,
+                    acc.getApi(),
+                    acc.getDeviceKind());
+        }
+
+    } // namespace onAcc::internal
+
+    namespace internal
     {
-        return trait::AutoIndexMapping::
-            Op<ALPAKA_TYPEOF(acc), ALPAKA_TYPEOF(acc.getApi()), ALPAKA_TYPEOF(acc.getDeviceKind())>{}(
-                acc,
-                acc.getApi(),
-                acc.getDeviceKind());
-    }
+        /** Specialize the trait for DataSource class if the object is copyable.
+         *
+         * @tparam TDataSource The DataSource class.
+         *
+         * @details
+         *
+         * The trait is used in the alpaka::internal::concepts::CopyConstructableDataSource concept to check whether
+         * the copy constructor respects the const correctness of the data type.
+         *
+         * Example specialization:
+         *
+         * @code
+         * template<typename T_Type>
+         * struct CopyConstructableDataSource<Storage<T_Type> : std::true_type {
+         *      using InnerMutable = Storage<std::remove_const_t<T_Type>>;
+         *      using InnerConst = Storage<std::add_const_t<T_Type>>;
+         * };
+         * @endcode
+         */
+        template<typename TDataSource>
+        struct CopyConstructableDataSource : std::false_type
+        {
+        };
 
-} // namespace alpaka::onAcc::internal
+    }; // namespace internal
+} // namespace alpaka
