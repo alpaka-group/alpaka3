5b8caeb82626bcd4f5d234ec410b5538130bf57b
mehmet yusufoglu <mehmetyusufoglu01@gmail.com>
2025-11-11 15:45:50 +0100

    remove /unifiedCudaHip/warp.hpp

--- include/alpaka/api/unifiedCudaHip/warp.hpp
+++
@@ -1,8 +0,0 @@
-/* Copyright 2025 Mehmet Yusufoglu, René Widera
- * Aggregates the backend-specific warp intrinsics so legacy include paths keep working.
- */
-
-#pragma once
-
-#include "alpaka/api/cuda/warp.hpp"
-#include "alpaka/api/hip/warp.hpp"
