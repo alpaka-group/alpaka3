fc1e150ccb88fbc865b4628eb080be6c98bed49a
AntonReinhard <anton.reinhard@proton.me>
2025-12-15 16:39:12 +0100

    Swap argument orders to (..., [buf], output, input)
    Remove template arguments from scan

--- include/alpaka/onHost/algo/scan.hpp
+++ include/alpaka/onHost/algo/scan.hpp
@@ -19,144 +19,131 @@ namespace alpaka::onHost
      * @param extents The extents of the scan.
      * @return The size of the buffer to allocate in number of elements. The buffer must have the same data type as the
      * object being scanned.
+     *
+     * TODO: in bytes, require element type
      */
-    auto getScanBufferSize(alpaka::concepts::Vector auto const& extents)
+    auto getScanBufferSize(alpaka::concepts::VectorOrScalar auto const& extents)
     {
         return internal::scanBufferSize(extents);
     }
 
     /** @brief Perform an inclusive scan on the input data and write the result to the output data.
      *
-     * @tparam T_Data Data type that will be scanned. T_Data{0} should be the neutral element for addition.
      * @param exec The executor to run with.
-     * @param devAcc The device to run on.
+     * @param devAcc The device to run on. TODO: remove
      * @param queue The queue to enqueue to.
-     * @param inputVec The input data. Can be const.
-     * @param outputVec The output data. To perform an in-place scan, use the overload with only one data object.
      * @param buffer (optional) The internally used buffer. Use the scanBufferSize() function to check how big the
      * buffer needs to be. If omitted, it will be allocated and destructed on the fly.
+     * @param outputVec The output data. To perform an in-place scan, use the overload with only one data object.
+     * @param inputVec The input data. Can be const.
+     *
+     * TODO:
      */
-    template<typename T_Data>
     void inclusiveScan(
         alpaka::concepts::Executor auto& exec,
         alpaka::onHost::concepts::Device auto& devAcc,
         auto const& queue,
-        alpaka::concepts::IDataSource<T_Data> auto const& inputVec,
-        alpaka::concepts::IMdSpan<T_Data> auto outputVec,
-        auto... buffer)
+        alpaka::concepts::IMdSpan auto& buffer,
+        alpaka::concepts::IMdSpan auto& outputVec,
+        alpaka::concepts::IDataSource auto const& inputVec)
     {
-        using T_Idx = ALPAKA_TYPEOF(inputVec.getExtents())::type;
+        internal::scan<internal::INCLUSIVE_SCAN>(exec, devAcc, queue, buffer, outputVec, inputVec);
+    }
 
-        if constexpr(sizeof...(buffer))
-        {
-            // use the passed buffer
-            auto _buffer = std::get<0>(std::make_tuple(buffer...));
-            internal::scan<internal::INCLUSIVE_SCAN, T_Idx, T_Data>(exec, devAcc, queue, inputVec, outputVec, _buffer);
-        }
-        else
-        {
-            // buffer will be allocated on the fly
-            internal::scan<internal::INCLUSIVE_SCAN, T_Idx, T_Data>(exec, devAcc, queue, inputVec, outputVec);
-        }
+    void inclusiveScan(
+        alpaka::concepts::Executor auto& exec,
+        alpaka::onHost::concepts::Device auto& devAcc,
+        auto const& queue,
+        alpaka::concepts::IMdSpan auto& outputVec,
+        alpaka::concepts::IDataSource auto const& inputVec)
+    {
+        internal::scan<internal::INCLUSIVE_SCAN>(exec, devAcc, queue, outputVec, inputVec);
     }
 
     /** @brief Perform an inclusive scan on data in-place.
      *
-     * @tparam T_Data Data type that will be scanned. T_Data{0} should be the neutral element for addition.
      * @param exec The executor to run with.
      * @param devAcc The device to run on.
      * @param queue The queue to enqueue to.
-     * @param dataVec The vector to scan, will be overwritten with the result.
      * @param buffer (optional) The internally used buffer. Use the scanBufferSize() function to check how big the
      * buffer needs to be. If omitted, it will be allocated and destructed on the fly.
+     * @param dataVec The vector to scan, will be overwritten with the result.
      */
-    template<typename T_Data>
     void inclusiveScanInPlace(
         alpaka::concepts::Executor auto& exec,
         alpaka::onHost::concepts::Device auto& devAcc,
         auto const& queue,
-        alpaka::concepts::IMdSpan<T_Data> auto& dataVec,
-        auto... buffer)
+        alpaka::concepts::IMdSpan auto& buffer,
+        alpaka::concepts::IMdSpan auto& dataVec)
     {
-        using T_Idx = ALPAKA_TYPEOF(dataVec.getExtents())::type;
+        internal::scan<internal::INCLUSIVE_SCAN>(exec, devAcc, queue, buffer, dataVec, dataVec);
+    }
 
-        if constexpr(sizeof...(buffer))
-        {
-            // use the passed buffer
-            auto _buffer = std::get<0>(std::make_tuple(buffer...));
-            internal::scan<internal::INCLUSIVE_SCAN, T_Idx, T_Data>(exec, devAcc, queue, dataVec, dataVec, _buffer);
-        }
-        else
-        {
-            // buffer will be allocated on the fly
-            internal::scan<internal::INCLUSIVE_SCAN, T_Idx, T_Data>(exec, devAcc, queue, dataVec, dataVec);
-        }
+    void inclusiveScanInPlace(
+        alpaka::concepts::Executor auto& exec,
+        alpaka::onHost::concepts::Device auto& devAcc,
+        auto const& queue,
+        alpaka::concepts::IMdSpan auto& dataVec)
+    {
+        internal::scan<internal::INCLUSIVE_SCAN>(exec, devAcc, queue, dataVec, dataVec);
     }
 
     /** @brief Perform an exclusive scan on the input data and write the result to the output data.
      *
-     * @tparam T_Data Data type that will be scanned. T_Data{0} should be the neutral element for addition.
      * @param exec The executor to run with.
      * @param devAcc The device to run on.
      * @param queue The queue to enqueue to.
-     * @param inputVec The input data. Can be const.
-     * @param outputVec The output data. To perform an in-place scan, use the overload with only one data object.
      * @param buffer (optional) The internally used buffer. Use the scanBufferSize() function to check how big the
      * buffer needs to be. If omitted, it will be allocated and destructed on the fly.
+     * @param outputVec The output data. To perform an in-place scan, use the overload with only one data object.
+     * @param inputVec The input data. Can be const.
      */
-    template<typename T_Data>
     void exclusiveScan(
         alpaka::concepts::Executor auto& exec,
         alpaka::onHost::concepts::Device auto& devAcc,
         auto const& queue,
-        alpaka::concepts::IDataSource<T_Data> auto const& inputVec,
-        alpaka::concepts::IMdSpan<T_Data> auto outputVec,
-        auto... buffer)
+        alpaka::concepts::IMdSpan auto& buffer,
+        alpaka::concepts::IMdSpan auto& outputVec,
+        alpaka::concepts::IDataSource auto const& inputVec)
     {
-        using T_Idx = ALPAKA_TYPEOF(inputVec.getExtents())::type;
+        internal::scan<internal::EXCLUSIVE_SCAN>(exec, devAcc, queue, buffer, outputVec, inputVec);
+    }
 
-        if constexpr(sizeof...(buffer))
-        {
-            // use the passed buffer
-            auto _buffer = std::get<0>(std::make_tuple(buffer...));
-            internal::scan<internal::EXCLUSIVE_SCAN, T_Idx, T_Data>(exec, devAcc, queue, inputVec, outputVec, _buffer);
-        }
-        else
-        {
-            // buffer will be allocated on the fly
-            internal::scan<internal::EXCLUSIVE_SCAN, T_Idx, T_Data>(exec, devAcc, queue, inputVec, outputVec);
-        }
+    void exclusiveScan(
+        alpaka::concepts::Executor auto& exec,
+        alpaka::onHost::concepts::Device auto& devAcc,
+        auto const& queue,
+        alpaka::concepts::IMdSpan auto& outputVec,
+        alpaka::concepts::IDataSource auto const& inputVec)
+    {
+        internal::scan<internal::EXCLUSIVE_SCAN>(exec, devAcc, queue, outputVec, inputVec);
     }
 
     /** @brief Perform an exclusive scan on data in-place.
      *
-     * @tparam T_Data Data type that will be scanned. T_Data{0} should be the neutral element for addition.
      * @param exec The executor to run with.
      * @param devAcc The device to run on.
      * @param queue The queue to enqueue to.
-     * @param dataVec The vector to scan, will be overwritten with the result.
      * @param buffer (optional) The internally used buffer. Use the scanBufferSize() function to check how big the
      * buffer needs to be. If omitted, it will be allocated and destructed on the fly.
+     * @param dataVec The vector to scan, will be overwritten with the result.
      */
-    template<typename T_Data>
     void exclusiveScanInPlace(
         alpaka::concepts::Executor auto& exec,
         alpaka::onHost::concepts::Device auto& devAcc,
         auto const& queue,
-        alpaka::concepts::IMdSpan<T_Data> auto& dataVec,
-        auto... buffer)
+        alpaka::concepts::IMdSpan auto& buffer,
+        alpaka::concepts::IMdSpan auto& dataVec)
     {
-        using T_Idx = ALPAKA_TYPEOF(dataVec.getExtents())::type;
-        if constexpr(sizeof...(buffer))
-        {
-            // use the passed buffer
-            auto _buffer = std::get<0>(std::make_tuple(buffer...));
-            internal::scan<internal::EXCLUSIVE_SCAN, T_Idx, T_Data>(exec, devAcc, queue, dataVec, dataVec, _buffer);
-        }
-        else
-        {
-            // buffer will be allocated on the fly
-            internal::scan<internal::EXCLUSIVE_SCAN, T_Idx, T_Data>(exec, devAcc, queue, dataVec, dataVec);
-        }
+        internal::scan<internal::EXCLUSIVE_SCAN>(exec, devAcc, queue, buffer, dataVec, dataVec);
+    }
+
+    void exclusiveScanInPlace(
+        alpaka::concepts::Executor auto& exec,
+        alpaka::onHost::concepts::Device auto& devAcc,
+        auto const& queue,
+        alpaka::concepts::IMdSpan auto& dataVec)
+    {
+        internal::scan<internal::EXCLUSIVE_SCAN>(exec, devAcc, queue, dataVec, dataVec);
     }
 } // namespace alpaka::onHost
