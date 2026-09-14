332dfc5e2afc39eba615b4cba4e5aa32ed434340
René Widera <r.widera@hzdr.de>
2026-09-14 17:55:45 +0200

    CI: add support for GNU 16

--- script/job_generator/src/alpaka_bashi/versions.py
+++ script/job_generator/src/alpaka_bashi/versions.py
@@ -37,7 +37,7 @@ from bashi.version.dependencies.nvcc import NVCC_GCC_MAX_VERSION, NvccHostSuppor
 from alpaka_bashi.globals import BUILD_TYPE, BUILD_TYPES, HWLOC
 
 ALPAKA_VERSIONS: dict[str, list[str | int | float]] = {
-    GCC: [12, 13, 14, 15],
+    GCC: [12, 13, 14, 15, 16],
     CLANG: [17, 18, 19, 20, 21],
     NVCC: [12.5, 12.6, 12.8, 12.9, 13.0, 13.1, 13.2, 13.3, 13.4],
     HIPCC: [6.3, 6.4, 7.0, 7.1, 7.2, 7.14, 10.0],
